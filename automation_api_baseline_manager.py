8244c2ff3d439750c7c1557d0ea9db4df95db28f
niteshkumar93 <nitesh.kumar@provartesting.com>
2025-12-30 13:54:42 +0530

    RemoveAutomation

--- automation_api_baseline_manager.py
+++ automation_api_baseline_manager.py
@@ -5,9 +5,9 @@ from typing import List, Dict
 # BASELINE LIST (KNOWN PROJECTS)
 # -----------------------------------------------------------
 KNOWN_PROJECTS = [
-    "AutomationAPI_LightningLWC",
-    "AutomationAPI_Flexi5",
-    "AutomationAPI_Flexi3"
+    "LightningLWC",
+    "Flexi5",
+    "Flexi3"
 ]
 # Separate baseline directory for AutomationAPI
 BASELINE_DIR = "baselines/automation_api"
